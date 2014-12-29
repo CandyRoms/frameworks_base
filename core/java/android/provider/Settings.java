b1e4dd188f768e1899448793ef4f865e93567a76
Stevespear426 <stephen.k.spear@gmail.com>
2014-12-29 16:03:42 -0200

    AOKP custom system animations (1/2)
    - Work by AOKP team and ported by @cristianomatos @crdroidandroid
    
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    
    Change-Id: Ibeaa0f369279bfa94f2547c423483f7eff80bcb8

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4483,6 +4483,24 @@ public final class Settings {
          */
         public static final String SCREENSHOT_EDIT_USER_APP = "screenshot_edit_user_app";
 
+        /**  
+         * AOKP Custom System Animations
+         * @hide
+         */  
+        public static final String[] ACTIVITY_ANIMATION_CONTROLS = new String[] {  
+                "activity_open",
+                "activity_close",
+                "task_open",  
+                "task_close",  
+                "task_to_front",
+                "task_to_back",  
+                "wallpaper_open",  
+                "wallpaper_close",  
+                "wallpaper_intra_open",  
+                "wallpaper_intra_close",  
+        };  
+        public static final String ANIMATION_CONTROLS_DURATION = "animation_controls_duration";
+
         /**
          * Whether to use gesture anywhere feature.
          * @hide
