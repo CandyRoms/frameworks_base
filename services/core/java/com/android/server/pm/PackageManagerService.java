443747e83f92fba51d7be492c74b803adc0f2fd3
Roman Birg <roman@cyngn.com>
2015-06-10 15:41:46 -0700

    Explicitly declare which permissions can be granted via signature
    This moves the older <allow-permissions> implementation inline with the
    newer one, which requires any permission that can be granted via a
    signature to explicitly add the attribute allowViaWhitelist="true".
    
    Change-Id: Ie9f9bf6e2138075b750c58090dcd7030eff174f1
    Signed-off-by: Roman Birg <roman@cyngn.com>
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -8640,8 +8640,7 @@ public class PackageManagerService extends IPackageManager.Stub {
                         == PackageManager.SIGNATURE_MATCH);
         if (!allowed && (bp.protectionLevel
                 & PermissionInfo.PROTECTION_FLAG_PRIVILEGED) != 0) {
-            boolean allowedSig = isAllowedSignature(pkg, perm);
-            if (isSystemApp(pkg) || allowedSig) {
+            if (isSystemApp(pkg)) {
                 // For updated system applications, a system permission
                 if (pkg.isUpdatedSystemApp()) {
                     final PackageSetting sysPs = mSettings
@@ -8672,7 +8671,7 @@ public class PackageManagerService extends IPackageManager.Stub {
                         }
                     }
                 } else {
-                    allowed = isPrivilegedApp(pkg) || allowedSig;
+                    allowed = isPrivilegedApp(pkg);
                 }
             }
         }
