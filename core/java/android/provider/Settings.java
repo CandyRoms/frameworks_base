a075c3f1bb2ce07cf5cda7741864e4b35d5ce477
gmillz <griffinn.millender@gmail.com>
2015-10-24 21:56:25 -0400

    Dotted Battery icon style [1/2] @BeansTown106: tweaked it to play nice with cm's cleaned up battery handling

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3517,6 +3517,7 @@ public final class Settings {
          * Display style of the status bar battery information
          * 0: Display the battery an icon in portrait mode
          * 2: Display the battery as a circle
+         * 3: Display the battery as a dotted circle
          * 4: Hide the battery status information
          * 5: Display the battery an icon in landscape mode
          * 6: Display the battery as plain text
