a0da5a341abef09dd4c064d2eff27d70b83c05c4
DvTonder <david.vantonder@gmail.com>
2015-11-16 21:47:55 +0100

    Frameworks: Allow/Prevent notification light in Zen mode (1 of 2)
    This allows the user to prevent the notification lights
    from showing during Zen mode as per Android 5.0.x
    
    Change-Id: I70519d654a8dfc8dba555fa6ea0df3b657dc286d
    
    Conflicts:
    	core/java/android/provider/Settings.java
    
    Conflicts:
    	core/java/android/provider/Settings.java
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3660,6 +3660,7 @@ public final class Settings {
          */
 
         /**
+<<<<<<< HEAD
          * Whether to show the battery bar
          * @hide
          */
@@ -3684,6 +3685,12 @@ public final class Settings {
          * @hide
          */
         public static final String STATUSBAR_BATTERY_BAR_ANIMATE = "statusbar_battery_bar_animate";
+=======
+         * Whether the notification light will be allowed when in zen mode during downtime
+         * @hide
+         */
+        public static final String ALLOW_LIGHTS = "allow_lights";
+>>>>>>> fc2f2c9... Frameworks: Allow/Prevent notification light in Zen mode (1 of 2)
 
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
@@ -3739,7 +3746,8 @@ public final class Settings {
             NOTIFICATION_SOUND,
             WIFI_AUTO_CONNECT_TYPE,
             STATUS_BAR_SHOW_BATTERY_PERCENT,
-            STATUS_BAR_BATTERY_STYLE
+            STATUS_BAR_BATTERY_STYLE,
+            ALLOW_LIGHTS,
         };
 
         /**
