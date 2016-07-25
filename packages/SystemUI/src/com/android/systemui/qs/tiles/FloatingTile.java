AOSPA/android_frameworks_base-old

SystemUI: Add qs tile for floating peek
Change-Id: I19c581e8fcad60ad3d759dbabcf8b8846d93e3fe

packages/SystemUI/src/com/android/systemui/qs/tiles/FloatingTile.java
@@ -1,0 +1,109 @@
+/*
+ * Copyright (C) 2016 The ParanoidAndroid Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.qs.tiles;
+
+import android.content.Context;
+import android.content.Intent;
+import android.provider.Settings;
+import android.provider.Settings.Secure;
+
+import com.android.internal.logging.MetricsLogger;
+import com.android.systemui.qs.SecureSetting;
+import com.android.systemui.qs.QSTile;
+import com.android.systemui.R;
+
+public class FloatingTile extends QSTile<QSTile.BooleanState> {
+    public static final String SPEC = "floating";
+
+    private final SecureSetting mSetting;
+
+    public FloatingTile(Host host) {
+        super(host, SPEC);
+
+        mSetting = new SecureSetting(mContext, mHandler, Secure.FLOATING_HEADSUP) {
+            @Override
+            protected void handleValueChanged(int value, boolean observedChange) {
+                handleRefreshState(value);
+            }
+        };
+    }
+
+    @Override
+    protected BooleanState newTileState() {
+        return new BooleanState();
+    }
+
+    @Override
+    protected void handleToggleClick() {
+        setEnabled(!mState.value);
+        refreshState();
+    }
+
+    @Override
+    protected void handleDetailClick() {
+        // TODO: Add proper detail view
+        handleToggleClick();
+    }
+
+    @Override
+    protected void handleLongClick() {
+        // Do nothing
+    }
+
+    private void setEnabled(boolean enabled) {
+        mSetting.setValue(enabled ? 1 : 0);
+    }
+
+    @Override
+    protected void handleUpdateState(BooleanState state, Object arg) {
+        final int value = arg instanceof Integer ? (Integer)arg : mSetting.getValue();
+        final boolean enable = value != 0;
+        state.value = enable;
+        state.visible = true;
+        if (enable) {
+            state.icon = ResourceIcon.get(R.drawable.ic_qs_floating_on);
+            state.label = mContext.getString(
+                    R.string.qs_floating_headsup_on);
+        } else {
+            // TODO: add icon for disabled state
+            state.icon = ResourceIcon.get(R.drawable.ic_qs_floating_on);
+            state.label =  mContext.getString(
+                    R.string.qs_floating_headsup_off);
+        }
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsLogger.QS_FLOATING;
+    }
+
+    @Override
+    protected String composeChangeAnnouncement() {
+        if (mState.value) {
+            return mContext.getString(
+                    R.string.qs_floating_headsup_on);
+        } else {
+            return mContext.getString(
+                    R.string.qs_floating_headsup_off);
+        }
+    }
+
+    @Override
+    public void setListening(boolean listening) {
+        // Do nothing
+    }
+}

CandyRoms/frameworks_base

packages/SystemUI/src/com/android/systemui/qs/tiles/FloatingTile.java
@@ -1,0 +1,109 @@
+/*
+ * Copyright (C) 2016 The ParanoidAndroid Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.qs.tiles;
+
+import android.content.Context;
+import android.content.Intent;
+import android.provider.Settings;
+import android.provider.Settings.Secure;
+
+import com.android.internal.logging.MetricsLogger;
+import com.android.systemui.qs.SecureSetting;
+import com.android.systemui.qs.QSTile;
+import com.android.systemui.R;
+
+public class FloatingTile extends QSTile<QSTile.BooleanState> {
+    public static final String TAG = "FloatingTile";
+
+    private final SecureSetting mSetting;
+
+    public FloatingTile(Host host) {
+        super(host);
+
+        mSetting = new SecureSetting(mContext, mHandler, Secure.FLOATING_HEADSUP) {
+            @Override
+            protected void handleValueChanged(int value, boolean observedChange) {
+                handleRefreshState(value);
+            }
+        };
+    }
+
+    @Override
+    protected BooleanState newTileState() {
+        return new BooleanState();
+    }
+
+    @Override
+    protected void handleClick() {
+        setEnabled(!mState.value);
+        refreshState();
+    }
+
+    // @Override
+    protected void handleDetailClick() {
+        // TODO: Add proper detail view
+        handleClick();
+    }
+
+    @Override
+    protected void handleLongClick() {
+        // Do nothing
+    }
+
+    private void setEnabled(boolean enabled) {
+        mSetting.setValue(enabled ? 1 : 0);
+    }
+
+    @Override
+    protected void handleUpdateState(BooleanState state, Object arg) {
+        final int value = arg instanceof Integer ? (Integer)arg : mSetting.getValue();
+        final boolean enable = value != 0;
+        state.value = enable;
+        state.visible = true;
+        if (enable) {
+            state.icon = ResourceIcon.get(R.drawable.ic_qs_floating_on);
+            state.label = mContext.getString(
+                    R.string.qs_floating_headsup_on);
+        } else {
+            // TODO: add icon for disabled state
+            state.icon = ResourceIcon.get(R.drawable.ic_qs_floating_on);
+            state.label =  mContext.getString(
+                    R.string.qs_floating_headsup_off);
+        }
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsLogger.QS_FLOATING;
+    }
+
+    @Override
+    protected String composeChangeAnnouncement() {
+        if (mState.value) {
+            return mContext.getString(
+                    R.string.qs_floating_headsup_on);
+        } else {
+            return mContext.getString(
+                    R.string.qs_floating_headsup_off);
+        }
+    }
+
+    @Override
+    public void setListening(boolean listening) {
+        // Do nothing
+    }
+}