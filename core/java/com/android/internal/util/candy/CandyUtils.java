b7354a105003c5dd76fde6f771585da24b3f99ff
maxwen <max.weninger@gmail.com>
2017-01-12 02:28:02 +0100

    CandyUtils: Add isAvailableApp method
    Change-Id: Iefdaf3c0b2d5db688f3a3f2f56fbf08b535d37e4
    Signed-off-by: Joe Maples <joe@frap129.org>

--- core/java/com/android/internal/util/candy/CandyUtils.java
+++ core/java/com/android/internal/util/candy/CandyUtils.java
@@ -78,7 +78,18 @@ public class CandyUtils {
             return mStatusBarService;
         }
     }
-
+    public static boolean isAvailableApp(String packageName, Context context) {
+       Context mContext = context;
+       final PackageManager pm = mContext.getPackageManager();
+       try {
+           pm.getPackageInfo(packageName, PackageManager.GET_ACTIVITIES);
+           int enabled = pm.getApplicationEnabledSetting(packageName);
+           return enabled != PackageManager.COMPONENT_ENABLED_STATE_DISABLED &&
+               enabled != PackageManager.COMPONENT_ENABLED_STATE_DISABLED_USER;
+       } catch (NameNotFoundException e) {
+           return false;
+       }
+    }
     public static boolean isChineseLanguage() {
        return Resources.getSystem().getConfiguration().locale.getLanguage().startsWith(
                Locale.CHINESE.getLanguage());
