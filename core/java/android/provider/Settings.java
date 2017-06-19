bc8f7e1d063596d858d5bf083eaeed685e284ecd
maxwen <max.weninger@gmail.com>
2017-06-19 17:13:49 +0200

    [1/2] base: config to disable power menu on secure lock screen
    Change-Id: Ie3f0b7c29fec17f1f392f95b46733494ac5466f9

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7334,6 +7334,13 @@ public final class Settings {
         public static final String STATUS_BAR_BATTERY_SAVER_COLOR = "status_bar_battery_saver_color";
 
         /**
+         * Disable power menu on secure lock screens
+         *
+         * @hide
+         */
+        public static final String LOCK_POWER_MENU_DISABLED = "lockscreen_power_menu_disabled";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
