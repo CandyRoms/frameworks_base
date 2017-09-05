f747d053fa021cf3fec27266e37dfe7f7817c0be
maxwen <max.weninger@gmail.com>
2017-09-05 20:36:26 +0200

    SystemUI: add CPU info overlay
    Change-Id: I42d609701182cf54fa88d43539a9fe4cc6fd4226

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -14851,6 +14851,11 @@ public final class Settings {
         public static final String DISABLE_TRANSITION_ANIMATIONS = "disable_transition_animations";
 
         /**
+         * @hide
+         */
+        public static final String SHOW_CPU_OVERLAY = "show_cpu_overlay";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
