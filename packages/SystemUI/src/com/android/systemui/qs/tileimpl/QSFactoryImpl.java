84f6e18b1666bbb03522509a450ec6ab5fda58f2
Keith Mok <kmok@cyngn.com>
2017-09-06 09:03:56 +0800

    SystemUI: add caffeine qs tile
    [xyyx] Adapt for P
    
    Change-Id: I49ff5e9fc458ecb0a16084563f058013a2c7aad3

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -32,6 +32,7 @@ import com.android.systemui.qs.tiles.BatterySaverTile;
 import com.android.systemui.qs.tiles.BluetoothTile;
 import com.android.systemui.qs.tiles.CastTile;
 import com.android.systemui.qs.tiles.CellularTile;
+import com.android.systemui.qs.tiles.CaffeineTile;
 import com.android.systemui.qs.tiles.ColorInversionTile;
 import com.android.systemui.qs.tiles.CompassTile;
 import com.android.systemui.qs.tiles.CPUInfoTile;
@@ -209,7 +210,9 @@ public class QSFactoryImpl implements QSFactory {
                  return new SyncTile(mHost);
             case "cpuinfo":
                 return mCPUInfoTileProvider.get();
-	}
+            case "caffeine":
+                return new CaffeineTile(mHost);
+        }
 
         // Intent tiles.
         if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(mHost, tileSpec);
