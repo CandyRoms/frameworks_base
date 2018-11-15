2ee3ee7478e2e261633be83c4f5f7ac1315926eb
ezio84 <brabus84@gmail.com>
2018-11-15 01:12:39 +0100

    Add big circle battery
    Change-Id: I71b523a6d0f73a720af62b2df9d88239d327e6bb

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -8402,8 +8402,10 @@ public final class Settings {
          * 0: Display the battery an icon in portrait mode
          * 2: Display the battery as a circle
          * 3: Display the battery as a dotted circle
+         * 4: Display the battery as a big circle
          * 5: Display the battery as text
          * 6: Do not display the battery
+         * 7: Display the battery as a big dotted circle
          * default: 0
          * @hide
          */
