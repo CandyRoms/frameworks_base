a4a48aaab760037eec895ae9b2e250071e9ca046
Dan Cartier <nospamdan1@gmail.com>
2018-11-16 09:06:49 -0500

    Disable hwkey remapping for now
    Change-Id: I83882b5d8c5164055d36c2e49b9fe315dba3d62e

--- packages/SystemUI/Android.mk
+++ packages/SystemUI/Android.mk
@@ -35,6 +35,7 @@ LOCAL_SRC_FILES := \
     $(call all-Iaidl-files-under, $(RELATIVE_FINGERPRINT_PATH))
 
 LOCAL_SRC_FILES += $(call all-java-files-under, ../../../../packages/apps/SmartNav/src)
+LOCAL_SRC_FILES += $(call all-java-files-under, ../../../../external/DUtils/src)
 
 LOCAL_STATIC_ANDROID_LIBRARIES := \
     SystemUIPluginLib \
@@ -62,8 +63,7 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
     guava
 
 LOCAL_JAVA_LIBRARIES := telephony-common \
-    android.car \
-    org.dirtyunicorns.utils
+    android.car
 
 LOCAL_PACKAGE_NAME := SystemUI
 LOCAL_PRIVATE_PLATFORM_APIS := true
