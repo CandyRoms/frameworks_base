1de66b237c4a6669221d38468ad2adf5ef9a1a95
ezio84 <brabus84@gmail.com>
2018-11-28 21:19:12 +0100

    Night Light brightness mode options [1/2]
    P adaption: now in P the brightness slider represents absolute brightness
    also for adaptive mode instead of a gamma adjustment like it was in
    Oreo, so we can always set SCREEN_BRIGHTNESS instead of SCREEN_AUTO_BRIGHTNESS_ADJ
    when adaptive.
    
    Change-Id: I12b4f4d0893ad85dcf5c7213fd42f8a6ba567767

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4595,6 +4595,18 @@ public final class Settings {
          */
         public static final String ANIM_TILE_INTERPOLATOR = "anim_tile_interpolator";
 
+
+        /**
+        * @hide
+         * Whether to set a lower brightness level when enabling night mode
+         * 0: Disabled
+         * 1: Set the brightness to a very low value
+         * 2: Set the brightness to a low value
+         * 3: Set the brightness to a medium value
+         * @hide
+         */
+        public static final String NIGHT_BRIGHTNESS_VALUE = "night_brightness_value";
+
         /**
          * Force an Ambient notification when a new media track is being played
          * 0 - disabled
