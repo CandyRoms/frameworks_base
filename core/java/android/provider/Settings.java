92e0d318189a0917ff4f0bd5bbe48770415ec4a6
Dan Cartier <nospamdan1@gmail.com>
2018-12-31 13:15:26 -0500

    Settings: Remove duplicate FULL_GESTURE_NAVBAR* tags
    Change-Id: Id0ca4b03b39d0717b463d54922e328fdf2b179a6

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4378,16 +4378,6 @@ public final class Settings {
         public static final String SCREENRECORD_QUALITY_MODE = "screenrecord_quality_mode";
 
         /**
-         * @hide
-         */
-        public static final String FULL_GESTURE_NAVBAR = "full_gesture_navbar";
-
-        /**
-         * @hide
-         */
-        public static final String FULL_GESTURE_NAVBAR_DT2S = "full_gesture_navbar_dt2s";
-
-        /**
          * enable or disable single handed mode
          * @hide
          */
