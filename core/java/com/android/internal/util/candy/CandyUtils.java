49f470fd5955a7438ade64521f531a1355db4490
Alex Cruz <du.alexcruz@gmail.com>
2019-01-13 11:10:54 -0600

    Method to detect a notch'd device
    Change-Id: I4742eaeea94c4495361bdfe2b855ae0de0aa24c1

--- core/java/com/android/internal/util/candy/CandyUtils.java
+++ core/java/com/android/internal/util/candy/CandyUtils.java
@@ -453,4 +453,23 @@ public class CandyUtils {
         return String.valueOf((n - c) % 2 == 0 ? (int) temp :
                 ForC ? c * 9/5 + 32 + "°F" :c + "°C");
     }
+
+    // Check if device has a notch
+    public static boolean hasNotch(Context context) {
+        int result = 0;
+        int resid;
+        int resourceId = context.getResources().getIdentifier(
+                "status_bar_height", "dimen", "android");
+        resid = context.getResources().getIdentifier("config_fillMainBuiltInDisplayCutout",
+                "bool", "android");
+        if (resid > 0) {
+            return context.getResources().getBoolean(resid);
+        }
+        if (resourceId > 0) {
+            result = context.getResources().getDimensionPixelSize(resourceId);
+        }
+        DisplayMetrics metrics = Resources.getSystem().getDisplayMetrics();
+        float px = 24 * (metrics.densityDpi / 160f);
+        return result > Math.round(px);
+    }
 }
