08a61e6d5c3694edeefdb6160c8d732fedd069d5
Dan Cartier <nospamdan1@gmail.com>
2019-03-19 20:26:55 -0400

    systemui: Add READ_PHONE_STATE and MODIFY_DAY_NIGHT_MODE perms, remove DUtils
    Change-Id: I8d5afbab9c06ed587d01017c8e560b065ebc0779

--- packages/SystemUI/Android.mk
+++ packages/SystemUI/Android.mk
@@ -35,7 +35,7 @@ LOCAL_SRC_FILES := \
     $(call all-Iaidl-files-under, $(RELATIVE_FINGERPRINT_PATH))
 
 LOCAL_SRC_FILES += $(call all-java-files-under, ../../../../packages/apps/SmartNav/src)
-#LOCAL_SRC_FILES += $(call all-java-files-under, ../../../../external/DUtils/src)
+LOCAL_SRC_FILES += $(call all-java-files-under, ../../../../external/google/src)
 
 LOCAL_STATIC_ANDROID_LIBRARIES := \
     SystemUIPluginLib \
@@ -75,6 +75,7 @@ LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res-keyguard $(LOCAL_PATH)/res $(LOCAL_PATH)/res-hwkeys
 LOCAL_RESOURCE_DIR += packages/apps/SmartNav/res
+LOCAL_RESOURCE_DIR += external/google/res
 
 ifneq ($(INCREMENTAL_BUILDS),)
     LOCAL_PROGUARD_ENABLED := disabled
