0fef9e3a0b6cd653b0139fe73de9cd6ff3fc1ac3
paphonb <paphonb@gmail.com>
2019-04-24 04:59:24 +0000

    [1/2] base: Add Q style to battery icon chooser
    Change-Id: Ia321ac7a078eb49b1f3967d59df81618d1672e83

--- packages/SettingsLib/src/com/android/settingslib/graph/BatteryMeterDrawableBase.java
+++ packages/SettingsLib/src/com/android/settingslib/graph/BatteryMeterDrawableBase.java
@@ -55,6 +55,7 @@ public class BatteryMeterDrawableBase extends Drawable {
     public static final int BATTERY_STYLE_TEXT = 5;
     public static final int BATTERY_STYLE_HIDDEN = 6;
     public static final int BATTERY_STYLE_BIG_DOTTED_CIRCLE = 7;
+    public static final int BATTERY_STYLE_Q = 8;
 
     protected final Context mContext;
     protected final Paint mFramePaint;
@@ -693,7 +694,7 @@ public class BatteryMeterDrawableBase extends Drawable {
     }
 
     protected float getAspectRatio() {
-        if (mMeterStyle != BATTERY_STYLE_PORTRAIT) {
+        if (mMeterStyle != BATTERY_STYLE_PORTRAIT && mMeterStyle != BATTERY_STYLE_Q) {
             return CIRCLE_ASPECT_RATIO;
         }
         return ASPECT_RATIO;
