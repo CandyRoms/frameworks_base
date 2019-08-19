3b9be65370ad013172d11d3bab3548be727d7384
Dan Cartier <nospamdan1@gmail.com>
2019-08-19 13:32:05 -0400

    Revert "FOD: Make sure we're only hiding fod on the normal keyguard"
    This reverts commit 528af420944e4beb51564e9d8352415c9e5f3522.
    
    Change-Id: I5c47d6eefcd1c93f4ea7d456afd3c8d14032b21a

--- packages/SystemUI/src/com/android/systemui/fingerprint/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/fingerprint/FODCircleView.java
@@ -77,7 +77,6 @@ public class FODCircleView extends ImageView implements OnTouchListener {
     private boolean mIsPulsing;
     private boolean mIsScreenOn;
     private boolean mShouldHide = false;
-    private boolean mIsKeyguard = false;
 
     public boolean viewAdded;
     private boolean mIsEnrolling;
@@ -134,7 +133,6 @@ public class FODCircleView extends ImageView implements OnTouchListener {
         public void onKeyguardVisibilityChanged(boolean showing) {
             super.onKeyguardVisibilityChanged(showing);
             mInsideCircle = false;
-            mIsKeyguard = true;
         }
 
         @Override
@@ -171,7 +169,7 @@ public class FODCircleView extends ImageView implements OnTouchListener {
         public void onTrustChanged(int userId) {
             super.onTrustChanged(userId);
             int mUserId = userId;
-            mShouldHide = (mIsKeyguard && mUpdateMonitor.getUserHasTrust(mUserId) ? true : false);
+            mShouldHide = (mUpdateMonitor.getUserHasTrust(mUserId) ? true : false);
         }
     };
 
