39b4589fbe53695f3925df6f5c2aa956f20c0c02
Dil3mm4 <dil3mm4.dev@gmail.com>
2020-01-11 19:20:59 +0100

    Screenrecord: correctly display notification title
    Change-Id: I220c29049de18ed670fa688a27898f57f40352af

--- packages/SystemUI/src/com/android/systemui/screenrecord/RecordingService.java
+++ packages/SystemUI/src/com/android/systemui/screenrecord/RecordingService.java
@@ -36,6 +36,7 @@ import android.media.MediaRecorder;
 import android.media.projection.MediaProjection;
 import android.media.projection.MediaProjectionManager;
 import android.net.Uri;
+import android.os.Bundle;
 import android.os.IBinder;
 import android.os.Handler;
 import android.os.Looper;
@@ -334,11 +335,14 @@ public class RecordingService extends Service {
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         notificationManager.createNotificationChannel(channel);
 
+        final Bundle extras = new Bundle();
+        extras.putString(Notification.EXTRA_SUBSTITUTE_APP_NAME, this.getString(R.string.global_action_screenrecord));
         mRecordingNotificationBuilder = new Notification.Builder(this, CHANNEL_ID)
                 .setSmallIcon(R.drawable.ic_sb_screenrecord)
                 .setContentTitle(getResources().getString(R.string.screenrecord_name))
                 .setUsesChronometer(true)
-                .setOngoing(true);
+                .setOngoing(true)
+                .addExtras(extras);
         setNotificationActions(false, notificationManager);
         Notification notification = mRecordingNotificationBuilder.build();
         startForeground(NOTIFICATION_ID, notification);
@@ -396,6 +400,8 @@ public class RecordingService extends Service {
                         PendingIntent.FLAG_UPDATE_CURRENT))
                 .build();
 
+        final Bundle extras = new Bundle();
+        extras.putString(Notification.EXTRA_SUBSTITUTE_APP_NAME, this.getString(R.string.global_action_screenrecord));
         Notification.Builder builder = new Notification.Builder(this, CHANNEL_ID)
                 .setSmallIcon(R.drawable.ic_sb_screenrecord)
                 .setContentTitle(getResources().getString(R.string.screenrecord_name))
@@ -407,7 +413,8 @@ public class RecordingService extends Service {
                         Intent.FLAG_GRANT_READ_URI_PERMISSION))
                 .addAction(shareAction)
                 .addAction(deleteAction)
-                .setAutoCancel(true);
+                .setAutoCancel(true)
+                .addExtras(extras);
 
         // Add thumbnail if available
         Bitmap thumbnailBitmap = null;
