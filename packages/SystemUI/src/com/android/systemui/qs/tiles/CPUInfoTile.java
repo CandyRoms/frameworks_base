462771aab75d8c9d6babfa15931d9e6007e8695e
ezio84 <brabus84@gmail.com>
2020-02-02 17:16:40 +0100

    SystemUI: Add Mono audio tile (aka headphone in one ear only)
    thanks buffal0b1ll for the suggestion!
    
    Change-Id: I284e6c3a1418dd88fa8457e59dffeabbfed93ceb
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    
    -----
    
    Mono/Stereo QS tile: Add long click intent
    
    Change-Id: I6fa456631b4513f4009e843d27bb7d2368d0506b
    
    -----
    
    MonoToggleTile: Use secondary label to indicate mode
    
    * Make default state inactive while we're at it
    
    Change-Id: I1aa1ec545743dd42c46996fa94bfc1c895a05a7c
    Co-authored-by: Alex <du.alexcruz@gmail.com>
    Co-authored-by: PainKiller3 <ninadpatil100@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/CPUInfoTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/CPUInfoTile.java
@@ -134,7 +134,7 @@ public class CPUInfoTile extends QSTileImpl<BooleanState> {
 
     @Override
     public int getMetricsCategory() {
-        return MetricsEvent.AWAKEN;
+        return MetricsEvent.CANDY;
     }
 
     @Override
