13c6185ccd34b8116f3774c0b403c30c46b5d650
ezio84 <brabus84@gmail.com>
2020-02-29 18:11:32 +0100

    Move launchCamera and launchVoiceSearch actions to custom Utils [1/2]
    Change-Id: I1ee720ece187509b77db1de9f671d75f95bec17a

--- core/java/com/android/internal/util/candy/CandyUtils.java
+++ core/java/com/android/internal/util/candy/CandyUtils.java
@@ -463,4 +463,16 @@ public class CandyUtils {
         AudioManager am = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
         am.adjustVolume(AudioManager.ADJUST_SAME, AudioManager.FLAG_SHOW_UI);
     }
+
+    public static void launchCamera(Context context) {
+        Intent intent = new Intent(MediaStore.INTENT_ACTION_STILL_IMAGE_CAMERA_SECURE);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        context.startActivity(intent);
+    }
+
+    public static void launchVoiceSearch(Context context) {
+        Intent intent = new Intent(Intent.ACTION_SEARCH_LONG_PRESS);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(intent);
+    }
 }
