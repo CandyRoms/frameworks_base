43e6f23bd0f220e587f3d3ef30231487ce32bf6f
LuK1337 <priv.luk@gmail.com>
2020-04-30 21:16:32 +0200

    FODCircleView: Let us call show() when mIsBouncer() && !isPinOrPattern()
    * Fixes missing fod circle when direct unlock is enabled.
    
    Change-Id: Ib4f8d97a50685cfc9e3a24c243352c7b1af736d0

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -304,8 +304,13 @@ public class FODCircleView extends ImageView {
     }
 
     public void show() {
-        if (mIsBouncer) {
-            // Ignore show calls when Keyguard pin screen is being shown
+        if (!mUpdateMonitor.isScreenOn()) {
+            // Keyguard is shown just after screen turning off
+            return;
+        }
+
+        if (mIsBouncer && !isPinOrPattern(mUpdateMonitor.getCurrentUser())) {
+            // Ignore show calls when Keyguard password screen is being shown
             return;
         }
 
