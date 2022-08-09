55d9746bf33928427ff77707cba73a9e79923f67
iusmac <iusico.maxim@libero.it>
2022-08-09 07:58:47 +0200

    Screenrecord: Allow to select AVC codec profile level
    Nevertheless we have an option to limit the recording by frame rate, but
    we still can't limit it by resolution. It's not particularly needed for
    the devices of today which are enough powerful and can encode/play any
    profile level, but there are still many old devices for which the
    profile level can make a big difference.
    
    Traditionally the device declares the high quality multimedia profiles
    for video encoding (editing, recording video via camera ecc.), but these
    profiles are also used for screen recording. And here comes the pitfall
    for weak devices: screen recording is a video encoding process +
    highload user tasks on foreground and background, all in total is too
    much for an old device. The recorded video is "laggy", because the
    frames are simply skipped to the detriment of system smoothness. That's
    why some devices will need to limit AVC codec profile level for screen
    recorder only, as it doesn't make sense to do it globally, because it
    will also affect camera and video editing.
    
    Test: record the screen using the closest profile level to the display
          capabilities (resolution@frame rate). Check if video max. resolution and
          frame rate match the selected profile level constraints [1]. In case the
          profile level is the right one, the quality and fluidity of the video
          won't be in any way different from what you normally see on your display
          and will be playable anywhere, including the device that recorded it.
    
    [1] https://en.wikipedia.org/wiki/Advanced_Video_Coding#Levels
    
    Signed-off-by: iusmac <iusico.maxim@libero.it>
    [@neobuddy89: Added default config to 4.2 and simplified!]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/screenrecord/ScreenMediaRecorder.java
+++ packages/SystemUI/src/com/android/systemui/screenrecord/ScreenMediaRecorder.java
@@ -53,6 +53,7 @@ import android.view.Surface;
 import android.view.WindowManager;
 
 import com.android.systemui.mediaprojection.MediaProjectionCaptureTarget;
+import com.android.systemui.res.R;
 
 import java.io.Closeable;
 import java.io.File;
@@ -90,6 +91,7 @@ public class ScreenMediaRecorder extends MediaProjection.Callback {
     private ScreenRecordingAudioSource mAudioSource;
     private final MediaProjectionCaptureTarget mCaptureRegion;
     private final Handler mHandler;
+    private String mAvcProfileLevel;
 
     private Context mContext;
     ScreenMediaRecorderListener mListener;
@@ -104,6 +106,8 @@ public class ScreenMediaRecorder extends MediaProjection.Callback {
         mCaptureRegion = captureRegion;
         mListener = listener;
         mAudioSource = audioSource;
+        mAvcProfileLevel = mContext.getResources().getString(
+                R.string.config_screenRecorderAVCProfileLevel);
     }
 
     private void prepare() throws IOException, RemoteException, RuntimeException {
@@ -151,7 +155,7 @@ public class ScreenMediaRecorder extends MediaProjection.Callback {
         mMediaRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.H264);
         mMediaRecorder.setVideoEncodingProfileLevel(
                 MediaCodecInfo.CodecProfileLevel.AVCProfileMain,
-                MediaCodecInfo.CodecProfileLevel.AVCLevel3);
+                getAvcProfileLevelCodeByName(mAvcProfileLevel));
         mMediaRecorder.setVideoSize(width, height);
         mMediaRecorder.setVideoFrameRate(refreshRate);
         mMediaRecorder.setVideoEncodingBitRate(vidBitRate);
@@ -194,6 +198,21 @@ public class ScreenMediaRecorder extends MediaProjection.Callback {
                     mMediaProjection, mAudioSource == MIC_AND_INTERNAL);
         }
 
+    }
+
+    /**
+     * Match human-readable AVC level name to its constant value.
+     */
+    private int getAvcProfileLevelCodeByName(final String levelName) {
+        switch (levelName) {
+            case "3": return MediaCodecInfo.CodecProfileLevel.AVCLevel3;
+            case "3.1": return MediaCodecInfo.CodecProfileLevel.AVCLevel31;
+            case "3.2": return MediaCodecInfo.CodecProfileLevel.AVCLevel32;
+            case "4": return MediaCodecInfo.CodecProfileLevel.AVCLevel4;
+            case "4.1": return MediaCodecInfo.CodecProfileLevel.AVCLevel41;
+            default:
+            case "4.2": return MediaCodecInfo.CodecProfileLevel.AVCLevel42;
+        }
     }
 
     /**
