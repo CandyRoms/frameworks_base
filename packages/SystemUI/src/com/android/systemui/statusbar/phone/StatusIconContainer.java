96907da4155a4e97c95885b08bd98c294a3da951
Ido Ben-Hur <idoybh2@gmail.com>
2023-04-19 18:32:49 +0300

    SystemUI: StatusIconContainer: Shorten entry animations
    Change-Id: Ic6d1e5f9b1f67ecd3fec77890df15b38485ac3ab

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusIconContainer.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusIconContainer.java
@@ -508,7 +508,7 @@ public class StatusIconContainer extends AlphaOptimizedLinearLayout {
         public AnimationFilter getAnimationFilter() {
             return mAnimationFilter;
         }
-    }.setDuration(200).setDelay(50);
+    }.setDuration(100);
 
     private static final AnimationProperties X_ANIMATION_PROPERTIES = new AnimationProperties() {
         private AnimationFilter mAnimationFilter = new AnimationFilter().animateX();
@@ -517,7 +517,7 @@ public class StatusIconContainer extends AlphaOptimizedLinearLayout {
         public AnimationFilter getAnimationFilter() {
             return mAnimationFilter;
         }
-    }.setDuration(200);
+    }.setDuration(100);
 
     private static final AnimationProperties ANIMATE_ALL_PROPERTIES = new AnimationProperties() {
         private AnimationFilter mAnimationFilter = new AnimationFilter().animateX().animateY()
@@ -527,5 +527,5 @@ public class StatusIconContainer extends AlphaOptimizedLinearLayout {
         public AnimationFilter getAnimationFilter() {
             return mAnimationFilter;
         }
-    }.setDuration(200);
+    }.setDuration(100);
 }
