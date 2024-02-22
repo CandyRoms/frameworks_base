690c16e3b9b99e590c617ae7974a307766ab275c
Pranav Vashi <neobuddy89@gmail.com>
2024-02-22 09:54:41 +0000

    CandyUtils: Refactor isPackageInstalled method
    Change-Id: Ia2ecdfd19353f39450721c575c369d3b197f804f
    Signed-off-by: afterallafk <shivamatiet2001@gmail.com>

--- core/java/com/android/internal/util/candy/CandyUtils.java
+++ core/java/com/android/internal/util/candy/CandyUtils.java
@@ -124,7 +124,7 @@ public class CandyUtils {
         if (packageName != null) {
             try {
                 PackageInfo pi = context.getPackageManager().getPackageInfo(packageName, 0);
-                if (!pi.applicationInfo.enabled && !ignoreState) {
+                if ((!pi.applicationInfo.enabled || !pi.applicationInfo.isProduct()) && !ignoreState) {
                     return false;
                 }
             } catch (PackageManager.NameNotFoundException e) {
